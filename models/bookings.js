91f43924c9fa2cf4bcba57425677805ce438b340
friedrich <friedrich.fertig360@gmail.com>
2026-09-23 10:50:45 +0200

    modification de la route booking pour transformer les trip en tableau d'objet

--- models/bookings.js
+++ models/bookings.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = mongoose.Schema({
-  trip: { type: mongoose.Schema.Types.ObjectId, ref: 'trips' },
+  trip: [{ type: mongoose.Schema.Types.ObjectId, ref: 'trips' }],
   booking: { type: Boolean},
 });
 
