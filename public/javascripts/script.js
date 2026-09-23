3f56472b51a6ecd081e41a47d8c5c3ddef13d4ed
Medhi Tache <medhi.tache@outlook.fr>
2026-09-23 09:35:09 +0200

    mise à jour

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -33,9 +33,8 @@ document.querySelector('#button').addEventListener('click', function() {
         data.Trips.forEach(trip => {
             result.innerHTML += `
                 <div class="line">
-                  <span class="departure">${trip.departure}</span>
+                  <span class="departure">${trip.departure}</span>>
                   <span class="arrival">${trip.arrival}</span>
-                  <span class="heure">${trip.date}</span>
                   <span class="heure">${trip.heure}</span>
                   <span class="price">${trip.price}€</span>
                   <a href="./cart.html?id=${trip._id}">Book</a>
